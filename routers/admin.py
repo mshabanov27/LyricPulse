8c23da30c2ebaba9a0d3aa90261e66509c41fd3f
Mitya <mityashabanov2004@gmail.com>
2023-12-28 21:26:58 +0400

    line length made smaller

--- routers/admin.py
+++ routers/admin.py
@@ -37,7 +37,8 @@ def search_users(request: Request, current_user: Annotated[User, Depends(get_cur
         for user in users:
             user_tags += f'<div class="user-piece"> \
                                 <div class="username">{user[1]}</div> \
-                                <button class ="black-button" id="{user[0]}" onclick="blockUser(this.id)"> Block </button> \
+                                <button class ="black-button" id="{user[0]}" onclick="blockUser(this.id)"> Block \
+                                </button> \
                             </div>'
     else:
         user_tags = '<div style="font-size: 40px; align-self: center;">Not found</div>'
@@ -60,7 +61,7 @@ def handle_empty_users(request: Request, current_user: Annotated[User, Depends(g
 
 
 @router.delete("/users/{userId}", tags=["Admin Actions"])
-def delete_other_user(request: Request, current_user: Annotated[User, Depends(get_current_user)], userId: str = Path()):
+def delete_other_user(current_user: Annotated[User, Depends(get_current_user)], userId: str = Path()):
     if not current_user.is_admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail='Available only for administrators')
